147f4df2ec6c89df9b8ffabc3956ee201051e7c9
Priyanshu Choudhary <57816400+Priyanchew@users.noreply.github.com>
2024-06-18 14:44:12 +0530

    Time: 854 ms (5.03%), Space: 45.5 MB (58.84%) - LeetHub

---
+++ 0853-most-profit-assigning-work/0853-most-profit-assigning-work.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
+        int total = 0;
+        for(int i=0; i<worker.length;i++){
+            int maxfori = 0;
+            for(int j=0; j<difficulty.length;j++){
+                if(difficulty[j]<=worker[i]){
+                    if(profit[j]>maxfori){
+                    maxfori=profit[j];
+                }
+                }
+            }
+            if(maxfori!=-1){total+=maxfori;}
+        }
+        return total;
+    }
+}
